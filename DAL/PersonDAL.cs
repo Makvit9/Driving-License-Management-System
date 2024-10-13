26de9368675db1c5cd62ff1fbc2704311fcf6a61
Makvit9 <crynged9@gmail.com>
2024-10-13 20:03:41 +0300

    Creating the Add Person Method

---
+++ DAL/PersonDAL.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class PersonDAL
+    {
+
+
+        // Basic Crud 
+
+        public static void AddNewPerson(string FirstName, string SecondName, string ThirdName, string LastName,
+            string NationalNumber, DateTime DateOfBirth, byte Gender, string Phone,
+            string Email,string Nationality, string Address )
+        {
+            SqlConnection connection = new SqlConnection(Settings.ConnectionString.connectionString);
+           
+            string Query = "INSERT INTO People " +
+                "VALUES ( @FirstName , @SecondName , @ThirdName , @LastName , @NationalNumber," +
+                "@DateOfBirth, @Gender, @Phone, @Email , @Nationality, @Address, ";
+
+            SqlCommand cmd = new SqlCommand(Query, connection);
+
+            cmd.Parameters.AddWithValue("@FirstName", FirstName);
+            cmd.Parameters.AddWithValue("@SecondName", SecondName);
+            cmd.Parameters.AddWithValue("@ThirdName", ThirdName);
+            cmd.Parameters.AddWithValue("@LastName", LastName);
+            cmd.Parameters.AddWithValue("@NationalNumber",NationalNumber );
+            cmd.Parameters.AddWithValue("@DateOfBirth", DateOfBirth);
+            cmd.Parameters.AddWithValue("@Gender", Gender);
+            cmd.Parameters.AddWithValue("@Phone", Phone);
+            cmd.Parameters.AddWithValue("@Email",Email );
+            cmd.Parameters.AddWithValue("@Nationality",Nationality );
+            cmd.Parameters.AddWithValue("@Address",Address );
+
+
+
+
+
+
+
+
+        }
+
+
+        public static void RemovePerson()
+        {
+
+        }
+
+
+        public static void UpdatePerson()
+        {
+
+        }
+
+        public static bool FindPerson(int NationalNumber)
+        {
+
+
+            return true;
+        }
+
+        public static void GetPersonInfo()
+        {
+
+        }
+    }
+}
---
+++ DAL/Settings/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Settings
+{
+    public class ConnectionString
+    {
+        public static string connectionString = "Server=.;database =ContactsDB;Trusted_Connection=True";
+    }
+}
