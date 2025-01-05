7a17979106da5832dd2e36c23af313b84692e653
Makvit9 <crynged9@gmail.com>
2025-01-06 02:59:13 +0300

    Creatign Test Types Class

---
+++ BL/TestType.cs
@@ -0,0 +1,47 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class TestType
+    {
+
+        public int TestTypeID { get; set; }
+        public string TestTypeTitle { get; set; }
+
+        public string TestTypeDescription { get; set; }
+
+
+        public decimal TestTypeFees { get; set; }
+
+        public static DataTable GetAllApplicationTypes()
+        {
+            return TestTypeDAL.GetAllTestTypes();
+        }
+
+        public static TestType GetTestTypeInformation(int TestTypeID)
+        {
+            string _TestTypeTitle = "";
+            string _TestTypeDescription = "";
+            decimal _TestTypeFees = -1;
+
+            if (TestTypeDAL.GetTestTypeInfo(TestTypeID, ref _TestTypeTitle, ref _TestTypeDescription ,ref _TestTypeFees))
+            {
+                return new TestType { TestTypeID = TestTypeID, TestTypeTitle = _TestTypeTitle, TestTypeDescription = _TestTypeDescription , TestTypeFees = _TestTypeFees };
+            }
+
+            return null;
+
+        }
+
+        public bool UpdateTestType()
+        {
+            return TestTypeDAL.UpdateTestType(this.TestTypeID, this.TestTypeTitle, this.TestTypeDescription,  this.TestTypeFees);
+        }
+    }
+}
