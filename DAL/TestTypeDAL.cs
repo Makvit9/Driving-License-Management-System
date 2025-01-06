676035cf96b074febcb89786c13a2ce2fe4003c7
Makvit9 <crynged9@gmail.com>
2025-01-06 03:11:16 +0300

    Creating Test Type Data Access Layer

---
+++ DAL/TestTypeDAL.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Settings;
+using Microsoft.SqlServer.Server;
+
+namespace DAL
+{
+    public class TestTypeDAL
+    {
+        public static DataTable GetAllTestTypes()
+        {
+
+            DataTable dt = new DataTable();
+
+            SqlConnection connection = new SqlConnection(Settings.ConnectionString.connectionString);
+
+            string Query = "Select * From TestTypes";
+
+            SqlCommand command = new SqlCommand(Query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            finally
+            {
+
+                connection.Close();
+            }
+            return dt;
+        }
+
+        public static bool GetTestTypeInfo(int TestTypeID, ref string TestTypeTitle,ref string TestTypeDescription, ref decimal TestTypeFees)
+        {
+            bool isFound = false;
+            SqlConnection connection = new SqlConnection(ConnectionString.connectionString);
+
+
+            string Query = @"SELECT * from TestTypes 
+                            WHERE  TestTypeID = @TestTypeID";
+
+
+            SqlCommand cmd = new SqlCommand(Query, connection);
+
+            cmd.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    // The record was found
+                    isFound = true;
+
+                    TestTypeTitle = (string)reader["TestTypeTitle"];
+                    TestTypeDescription = (string)reader["TestTypeDescription"];
+                    TestTypeFees = (Decimal)reader["TestTypeFees"];
+
+                    reader.Close();
+                }
+            }
+            catch (Exception)
+            {
+
+                isFound = false;
+            }
+            return isFound;
+        }
+
+        public static bool UpdateTestType(int TestTypeID, string TestTypeTitle, string TestTypeDescription, decimal TestTypeFees)
+        {
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(Settings.ConnectionString.connectionString);
+
+            string query = @"Update  TestTypes  
+                            set TestTypeTitle = @TestTypeTitle ,
+                                TestTypeDescription = @TestTypeDescription, 
+                                TestTypeFees = @TestTypeFees
+                                where TestTypeID = @TestTypeID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+            command.Parameters.AddWithValue("@TestTypeTitle", TestTypeTitle);
+            command.Parameters.AddWithValue("@TestTypeDescription", TestTypeDescription);
+            command.Parameters.AddWithValue("@TestTypeFees", TestTypeFees);
+
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
+    }
+}
