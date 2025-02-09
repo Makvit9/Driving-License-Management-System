8f213c8615f83f618744ee14640a2adf4997edea
Makvit9 <crynged9@gmail.com>
2025-02-09 04:46:01 +0300

    Creating Applications BL & DAL

---
+++ BL/ApplicationInfo.cs
@@ -0,0 +1,209 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class ApplicationInfo
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enum enApplicationType
+        {
+            NewDrivingLicense = 1, RetakeTest = 2 , RenewDrivingLicense = 3, ReplaceLostDrivingLicense = 4,
+            ReplaceDamagedDrivingLicense = 5, ReleaseDetainedDrivingLicsense = 6, NewInternationalLicense = 7
+        };
+
+
+        public enMode Mode = enMode.AddNew;
+        public enum enApplicationStatus { New = 1, Cancelled = 2, Completed = 3 };
+
+        public int ApplicationID { set; get; }
+        public int ApplicantPersonID { set; get; }
+
+
+        public DateTime ApplicationDate { set; get; }
+        public int ApplicationTypeID { set; get; }
+        public ApplicationType ApplicationTypeInfo;
+        public enApplicationStatus ApplicationStatus { set; get; }
+        public string StatusText
+        {
+            get
+            {
+
+                switch (ApplicationStatus)
+                {
+                    case enApplicationStatus.New:
+                        return "New";
+                    case enApplicationStatus.Cancelled:
+                        return "Cancelled";
+                    case enApplicationStatus.Completed:
+                        return "Completed";
+                    default:
+                        return "Unknown";
+                }
+            }
+
+        }
+
+
+        public DateTime LastStatusDate { set; get; }
+        public decimal PaidFees { set; get; }
+        public int CreatedByUserID { set; get; }
+        public User CreatedByUserInfo;
+
+        public ApplicationInfo()
+        {
+            this.ApplicationID = -1;
+            this.ApplicantPersonID = -1;
+            this.ApplicationDate = DateTime.Now;
+            this.ApplicationTypeID = -1;
+            this.ApplicationStatus = enApplicationStatus.New;
+            this.LastStatusDate = DateTime.Now;
+            this.PaidFees = 0;
+            this.CreatedByUserID = -1;
+
+            Mode = enMode.AddNew;
+        }
+
+        private ApplicationInfo(int ApplicationID, int ApplicantPersonID,
+            DateTime ApplicationDate, int ApplicationTypeID,
+             enApplicationStatus ApplicationStatus, DateTime LastStatusDate,
+             decimal PaidFees, int CreatedByUserID)
+
+        {
+            this.ApplicationID = ApplicationID;
+            this.ApplicantPersonID = ApplicantPersonID;
+            this.ApplicationDate = ApplicationDate;
+            this.ApplicationTypeID = ApplicationTypeID;
+            this.ApplicationTypeInfo = ApplicationType.GetApplicationTypeInformation(ApplicationTypeID);
+            this.ApplicationStatus = ApplicationStatus;
+            this.LastStatusDate = LastStatusDate;
+            this.PaidFees = PaidFees;
+            this.CreatedByUserID = CreatedByUserID;
+            this.CreatedByUserInfo = User.FindUser(CreatedByUserID);
+            Mode = enMode.Update;
+        }
+
+
+        private bool AddNewApplication()
+        {
+            this.ApplicationID = ApplicationInfoDAL.AddNewApplication(this.ApplicantPersonID, this.ApplicationDate,
+                this.ApplicationTypeID, (byte)this.ApplicationStatus, this.LastStatusDate, this.PaidFees, this.CreatedByUserID);
+
+            return this.ApplicationID != -1;
+        }
+
+        private bool UpdateApplication()
+        {
+            return ApplicationInfoDAL.UpdateApplication(this.ApplicationID, this.ApplicantPersonID, this.ApplicationDate, this.ApplicationTypeID,
+                (byte)this.ApplicationStatus, this.LastStatusDate, this.PaidFees, this.CreatedByUserID);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (AddNewApplication())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return UpdateApplication();
+
+            }
+
+            return false;
+        }
+
+
+        public bool Cancel()
+        {
+            return ApplicationInfoDAL.UpdateStatus(this.ApplicationID, (int)enApplicationStatus.Cancelled);
+        }
+
+        public bool Complete()
+        {
+            return ApplicationInfoDAL.UpdateStatus(this.ApplicationID, (int)enApplicationStatus.Completed);
+
+        }
+
+        public bool DeleteApplication()
+        {
+            return ApplicationInfoDAL.DeleteApplication(this.ApplicationID);
+        }
+
+
+        public static ApplicationInfo Find(int ApplicationID)
+        {
+            int ApplicantPersonID = -1;
+            DateTime ApplicationDate = DateTime.Now; int ApplicationTypeID = -1;
+            byte ApplicationStatus = 1; DateTime LastStatusDate = DateTime.Now;
+            decimal PaidFees = 0; int CreatedByUserID = -1;
+
+            bool IsFound = ApplicationInfoDAL.GetApplicationInfoByID
+                                (
+                                    ApplicationID, ref ApplicantPersonID,
+                                    ref ApplicationDate, ref ApplicationTypeID,
+                                    ref ApplicationStatus, ref LastStatusDate,
+                                    ref PaidFees, ref CreatedByUserID
+                                );
+
+            if (IsFound)
+            {
+                return new ApplicationInfo(ApplicationID, ApplicantPersonID,
+                                     ApplicationDate, ApplicationTypeID,
+                                    (enApplicationStatus)ApplicationStatus, LastStatusDate,
+                                     PaidFees, CreatedByUserID);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public static bool IsApplicationExists(int ApplicationID)
+        {
+            return ApplicationInfoDAL.IsApplicationExist(ApplicationID);
+        }
+        public bool DoesPersonHaveActiveApplication(int ApplicationTypeID)
+        {
+            return DoesPersonHaveActiveApplication(this.ApplicantPersonID, ApplicationTypeID);
+        }
+
+        public static bool DoesPersonHaveActiveApplication(int PersonID, int ApplicationTypeID)
+        {
+            return ApplicationInfoDAL.DoesPersonHaveActiveApplication(PersonID, ApplicationTypeID);
+        }
+
+
+        public static int GetActiveApplicationIDForLicenseClass(int PersonID, ApplicationInfo.enApplicationType ApplicationTypeID, int LicenseClassID)
+        {
+            return ApplicationInfoDAL.GetActiveApplicationIDForLicenseClass(PersonID, (int)ApplicationTypeID, LicenseClassID);
+        }
+
+        public int GetActiveApplicationID(ApplicationInfo.enApplicationType ApplicationTypeID)
+        {
+            return GetActiveApplicationID(this.ApplicantPersonID, ApplicationTypeID);
+        }
+
+        public static int GetActiveApplicationID(int PersonID, ApplicationInfo.enApplicationType ApplicationTypeID)
+        {
+            return ApplicationInfoDAL.GetActiveApplicationID(PersonID, (int)ApplicationTypeID);
+        }
+
+    }
+}
