7bfbe7454146e809761dcee7ba53784284a4fe9d
Makvit9 <crynged9@gmail.com>
2025-02-23 19:33:56 +0300

    Adding Class to TestAppointments & Creating the TestSchedule Control

--- BL/TestAppointment.cs
+++ BL/TestAppointment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,106 @@ namespace BL
 {
     public class TestAppointment
     {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int TestAppointmentID { set; get; }
+        public int TestTypeID { set; get; }
+        public int LocalDrivingLicenseApplicationID { set; get; }
+        public DateTime AppointmentDate { set; get; }
+        public Decimal PaidFees { set; get; }
+        public int CreatedByUserID { set; get; }
+        public bool IsLocked { set; get; }
+        public int RetakeTestApplicationID { set; get; }
+        public ApplicationInfo RetakeTestAppInfo { set; get; }
+
+        public int TestID
+        {
+            get { return _GetTestID(); }
+
+        }
+
+        public TestAppointment()
+
+        {
+            this.TestAppointmentID = -1;
+            this.TestTypeID = 1;
+            this.AppointmentDate = DateTime.Now;
+            this.PaidFees = 0;
+            this.CreatedByUserID = -1;
+            this.RetakeTestApplicationID = -1;
+            Mode = enMode.AddNew;
+
+        }
+
+        public TestAppointment(int TestAppointmentID, int TestTypeID,
+                               int LocalDrivingLicenseApplicationID, DateTime AppointmentDate, Decimal PaidFees,
+                               int CreatedByUserID, bool IsLocked, int RetakeTestApplicationID)
+
+        {
+            this.TestAppointmentID = TestAppointmentID;
+            this.TestTypeID = TestTypeID;
+            this.LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID;
+            this.AppointmentDate = AppointmentDate;
+            this.PaidFees = PaidFees;
+            this.CreatedByUserID = CreatedByUserID;
+            this.IsLocked = IsLocked;
+            this.RetakeTestApplicationID = RetakeTestApplicationID;
+            this.RetakeTestAppInfo = ApplicationInfo.Find(RetakeTestApplicationID);
+            Mode = enMode.Update;
+        }
+
+        private int _GetTestID()
+        {
+            return TestAppointmentDAL.GetTestID(TestAppointmentID);
+        }
+
+
+
+        private bool _AddNewTestAppointment()
+        {
+
+            this.TestAppointmentID = TestAppointmentDAL.AddNewTestAppointment((int)this.TestTypeID, this.LocalDrivingLicenseApplicationID,
+                this.AppointmentDate, this.PaidFees, this.CreatedByUserID, this.RetakeTestApplicationID);
+
+            return (this.TestAppointmentID != -1);
+        }
+
+        private bool _UpdateTestAppointment()
+        {
+        
+
+            return TestAppointmentDAL.UpdateTestAppointment(this.TestAppointmentID, (int)this.TestTypeID, this.LocalDrivingLicenseApplicationID,
+                this.AppointmentDate, this.PaidFees, this.CreatedByUserID, this.IsLocked, this.RetakeTestApplicationID);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewTestAppointment())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateTestAppointment();
+
+            }
+
+            return false;
+        }
+
+
+
 
     }
 }
